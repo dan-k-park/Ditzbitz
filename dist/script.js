733afd3cb06208ab0bb962b76a84b5586fe8c090
dan-k-park <daniel.kun.park@gmail.com>
2022-12-01 20:15:20 -1000

    first

---
+++ dist/script.js
@@ -0,0 +1,24 @@
+const primaryNav = document.querySelector('.primary-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  console.log(visibility);
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+});
+
+let resizeTimer;
+window.addEventListener('resize', () => {
+  document.body.classList.add('resize-animation-stopper');
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove('resize-animation-stopper');
+  }, 400);
+});
